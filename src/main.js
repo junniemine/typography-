59c7cb266e8a4b0f307110af19163436f6c4a72a
김한별 <gimhanbyeol@gimhanbyeol-ui-MacBookPro.local>
2025-12-08 02:34:53 +0900

    comment

--- src/main.js
+++ src/main.js
@@ -35,10 +35,10 @@ akTrigger.addEventListener('click', () => {
     msg.style.position = "fixed";
     msg.style.left = rect.right + 100 + "px";
     msg.style.top = rect.top + "px";
-    msg.style.fontSize = "2vw";
+    msg.style.fontSize = "3vw";
     msg.style.lineHeight = "1.6";
     msg.style.color = "white";
-    msg.style.transform = "translateY(-50%) scale(0.8)";
+    msg.style.transform = "translateY(-50%) scale(0.9)";
 
     inkOverlay.classList.add("active");
     inkOverlay.style.pointerEvents = "none";
@@ -165,9 +165,9 @@ document.addEventListener("mousemove", (e) => {
         msg2.style.left = "70%";
         msg2.style.top = "40%";
         msg2.style.color = "black";
-        msg2.style.fontSize = "2vw";
+        msg2.style.fontSize = "3vw";
         msg2.style.lineHeight = "1.6";
-        msg2.style.transform = "translateY(-50%) scale(0.8)";
+        msg2.style.transform = "translateY(-50%) scale(0.9)";
 
         msg.style.opacity = 0;
         return;
@@ -183,7 +183,7 @@ document.addEventListener("mousemove", (e) => {
         msg.style.left = "10%";
         msg.style.top = "40%";
         msg.style.color = "black";
-        msg.style.fontSize = "2vw";
+        msg.style.fontSize = "3vw";
         msg.style.lineHeight = "1.6";
 
         return;
