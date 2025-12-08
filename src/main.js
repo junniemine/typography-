fe4da0bad3712f0a9c64fbf5924cb98312aa4e56
김한별 <gimhanbyeol@gimhanbyeol-ui-MacBookPro.local>
2025-12-08 14:16:13 +0900

    comment

--- src/main.js
+++ src/main.js
@@ -37,7 +37,7 @@ akTrigger.addEventListener('click', () => {
     msg.style.position = "fixed";
     // put the message just to the right of the seon glyph, vertically centered
     msg.style.left = (rect.right + 40) + "px";
-    msg.style.top = (seonCenterY -10) + "px";
+    msg.style.top = (seonCenterY -15) + "px";
 
     // use viewport-relative font so GitHub deploy and local scale match better
     msg.style.fontSize = "1.2vw";
